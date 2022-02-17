2bc06b2d5df26caecafd5265fb3b709a9dd409a0
paolo-guebeli <guebeli.paolo@gmail.com>
2022-02-17 09:48:46 +0100

    Small fix and features csv

--- data_analisys/rm_test.py
+++ data_analisys/rm_test.py
@@ -11,20 +11,28 @@ from radiomics import featureextractor
 
 def get_features(path):
     dir_list = []
+    metastases = 0
     for dir in os.listdir(path):
         if '_full.nrrd' not in dir:
             if 'nrrd' in dir:
                 dir_list.append(dir)
     tdf = None
+    count = 0
     for folder in dir_list:
         extractor = featureextractor.RadiomicsFeatureExtractor("Params.yaml")
-        result = extractor.execute(path + '/' + folder + '_full.nrrd', path + '/' + folder + '/GTV-1.nrrd')
+        if os.path.exists(path + '/' + folder + '/GTV-1.nrrd'):
+            result = extractor.execute(path + '/' + folder + '_full.nrrd', path + '/' + folder + '/GTV-1.nrrd')
+        else:
+            for i in os.listdir( path + '/' + folder):
+                if 'gtv' in i:
+                    result = extractor.execute(path + '/' + folder + '_full.nrrd', path + '/' + folder + '/'+i)
+                    metastases -= 1
+                    break
         clean_dict = {}
         for key, value in result.items():
             if 'diagnostics' not in key:
                 value = float(value)
                 clean_dict[key] = value
-        metastases = 0
         for i in os.listdir(path + '/' + folder):
             if 'gtv' in i:
                 metastases += 1
@@ -32,13 +40,15 @@ def get_features(path):
         if tdf is None:
             tdf = pd.DataFrame(columns=clean_dict.keys())
         tdf = tdf.append(clean_dict, ignore_index=True)
+        count += 1
+        print(f'{count}/{len(dir_list)}')
 
     return tdf
 
 
 def main():
     tdf = get_features(path='dataset/manifest-1603198545583/NSCLC-Radiomics')
-    tdf.to_csv('features.csv')
+    tdf.to_csv('features.csv', sep=';')
 
 
 if __name__ == '__main__':

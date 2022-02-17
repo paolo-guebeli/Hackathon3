1fd93f7cbec57fc50ed3f27c1e2f1189e3a74a8a
paolo-guebeli <guebeli.paolo@gmail.com>
2022-02-17 08:48:40 +0100

    Added metastases counter

--- data_analisys/rm_test.py
+++ data_analisys/rm_test.py
@@ -10,7 +10,6 @@ from radiomics import featureextractor
 
 
 def get_features(path):
-    tdf_list = []
     dir_list = []
     for dir in os.listdir(path):
         if '_full.nrrd' not in dir:
@@ -19,20 +18,27 @@ def get_features(path):
     tdf = None
     for folder in dir_list:
         extractor = featureextractor.RadiomicsFeatureExtractor("Params.yaml")
-        result = extractor.execute(path+'/'+folder+'_full.nrrd', path+'/' + folder+'/GTV-1.nrrd')
+        result = extractor.execute(path + '/' + folder + '_full.nrrd', path + '/' + folder + '/GTV-1.nrrd')
         clean_dict = {}
         for key, value in result.items():
             if 'diagnostics' not in key:
                 value = float(value)
                 clean_dict[key] = value
+        metastases = 0
+        for i in os.listdir(path + '/' + folder):
+            if 'gtv' in i:
+                metastases += 1
+        clean_dict['metastases'] = metastases
         if tdf is None:
             tdf = pd.DataFrame(columns=clean_dict.keys())
         tdf = tdf.append(clean_dict, ignore_index=True)
+
     return tdf
 
 
 def main():
-    print(get_features('10_patients'))
+    tdf = get_features(path='dataset/manifest-1603198545583/NSCLC-Radiomics')
+    tdf.to_csv('features.csv')
 
 
 if __name__ == '__main__':

db7ce07a07dc9ad7e74e615874fc7a55c88ec16a
Giorgiovisco <giorgio.visconti01@gmail.com>
2022-02-18 10:20:05 +0100

    add prediction script
    add prediction script

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-import os, glob
+import os, glob, imp
 from tabnanny import filename_only
 from tkinter import filedialog
 from tkinter import *
@@ -11,55 +11,7 @@ import pylab as pl
 import sys
 import matplotlib.path as mplPath
 import one_time_features
-
-# set plotly credentials here 
-# this allows you to send results to your account plotly.tools.set_credentials_file(username=your_username, api_key=your_key
-#import SimpleITK as sitk
-#import six
-#import pandas as pd
-#import os
-
-#import radiomics as rm
-#from radiomics import featureextractor
-
-
-#def get_features(path):
-#    dir_list = []
-#    metastases = 0
-#    for dir in os.listdir(path):
-#        if '_full.nrrd' not in dir:
-#            if 'nrrd' in dir:
-#                dir_list.append(dir)
-#    tdf = None
-#    count = 0
-#    for folder in dir_list:
-#        extractor = featureextractor.RadiomicsFeatureExtractor("Params.yaml")
-#        if os.path.exists(path + '/' + folder + '/GTV-1.nrrd'):
-#            result = extractor.execute(path + '/' + folder + '_full.nrrd', path + '/' + folder + '/GTV-1.nrrd')
-#        else:
-#            for i in os.listdir( path + '/' + folder):
-#                if 'gtv' in i:
-#                    result = extractor.execute(path + '/' + folder + '_full.nrrd', path + '/' + folder + '/'+i)
-#                    metastases -= 1
-#                    break
-#        clean_dict = {}
-#        for key, value in result.items():
-#            if 'diagnostics' not in key:
-#                value = float(value)
-#                clean_dict[key] = value
-#        for i in os.listdir(path + '/' + folder):
-#            if 'gtv' in i:
-#                metastases += 1
-#        clean_dict['metastases'] = metastases
-#        if tdf is None:
-#            tdf = pd.DataFrame(columns=clean_dict.keys())
-#        tdf = tdf.append(clean_dict, ignore_index=True)
-#        count += 1
-#        print(f'{count}/{len(dir_list)}')
-
-#    return tdf
-
-#functions for dcm images display
+import pdfFunction
 
 
 def browse_button():
@@ -126,10 +78,8 @@ def view_image(path, img_path):
         fig, ax = plt.subplots(1,1)
         
         os.system("tree "+ path)
-        #os.system("tree C:/Users/giorg/Desktop/test python/LUNG1-001/09-18-2008-StudyID-NA-69331")
         plots = []
         for f in glob.glob(img_path + "/*.dcm"):
-        #for f in glob.glob("C:/Users/giorg/Desktop/test python/LUNG1-001/09-18-2008-StudyID-NA-69331/img/*.dcm"):
             if '1-1.dcm' not in f:
                 pass
                 ds = pydicom.dcmread(f)
@@ -149,7 +99,8 @@ def get_prediction(path):
     base_path = one_time_features.converter(path)
     features = one_time_features.get_features(base_path)
     # Add prediction script
-    # Add PDF creator
+    # pass data vector to pdf creator
+    pdfFunction.createPdf()
 
 
 if __name__ == '__main__':
---
+++ GUI/pdfFunction.py
@@ -0,0 +1,84 @@
+import datetime
+import subprocess
+import os
+import platform
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import inch
+from reportlab.lib import colors
+
+
+def createPdf(data_vector):
+    doc = SimpleDocTemplate("form_letter.pdf", pagesize=letter,
+                            rightMargin=72, leftMargin=72,
+                            topMargin=48, bottomMargin=18)
+
+    Story = []
+    logo = "cancerInstitute.jpg"
+
+    date = datetime.date.today()
+    department_name = "Supsi Cancer Department"
+    department_address = ["Via la Santa 1", "CH-6962 Lugano - Viganello"]
+
+    im = Image(logo, 2*inch, 2*inch)
+    Story.append(im)
+    Story.append(Spacer(1, 48))
+
+    styles = getSampleStyleSheet()
+    ptext = '%s' % date
+
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    Story.append(Spacer(1, 12))
+
+    ptext = '%s' % department_name
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    for part in department_address:
+        ptext = '%s' % part.strip()
+        Story.append(Paragraph(ptext, styles["Normal"]))
+
+    Story.append(Spacer(1, 48))
+    ptext = 'Dear Patient,'
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    Story.append(Spacer(1, 12))
+
+    ptext = 'After careful analysis of your data our department concluded the following:'
+
+    #vector = [111, '2', "abaghjaa", '4', '5']
+
+    data = [['Stage T', 'Stage N', 'Overall Stage', 'Istology', 'Life expectancy'],
+            [data_vector[0], data_vector[1], data_vector[2], data_vector[3], data_vector[4]]]
+    table = Table(data)
+    table.setStyle(TableStyle([('ALIGN', (1, 1), (-2, -2), 'RIGHT'),
+                               ('VALIGN', (0, 0), (0, -1), 'TOP'),
+                               ('ALIGN', (0, -1), (-1, -1), 'CENTER'),
+                               ('VALIGN', (0, -1), (-1, -1), 'MIDDLE'),
+                               ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                               ('BOX', (0, 0), (-1, -1), 0.25, colors.black)
+                               ]))
+
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    Story.append(Spacer(1, 36))
+    Story.append(table)
+
+
+    Story.append(Spacer(1, 60))
+
+    ptext = 'Hoping to have the ability to continue to help you the best we can.'
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    Story.append(Spacer(1, 36))
+    ptext = 'Sincerely,'
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    Story.append(Spacer(1, 12))
+    ptext = 'Doctor Name'
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    Story.append(Spacer(1, 12))
+    doc.build(Story)
+
+    # creates PDF file, different OS
+    if platform.system() == 'Windows':
+        os.startfile("form_letter.pdf")
+    elif platform.system() == 'Darwin':
+        subprocess.call(('open', "form_letter.pdf"))
+    else:
+        subprocess.call(('xdg-open', "form_letter.pdf"))
